151e495ef0f7fa85f393c59cc2f3956938fe8693
Rosana Đuga <rodjuga@gmail.com>
2019-10-25 19:54:23 +0200

    Added class Hours, two cshtmls for insert and view of all hours

---
+++ RadniSati/Controllers/RadniSatiController.cs
@@ -0,0 +1,39 @@
+﻿using RadniSati.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RadniSati.Controllers
+{
+    public class RadniSatiController : Controller
+    {
+        // GET: RadniSati
+        public ViewResult UnosSati()
+        {
+            return View();
+        }
+
+       
+
+        public ActionResult IspisSati(FormCollection form)
+        {
+            
+
+            ViewBag.ulazak = form["Ulazak"];
+            ViewBag.izlazak = form["Izlazak"];
+            DateTime date1 = DateTime.ParseExact(ViewBag.ulazak, "dd'.'MM'.'yyyy - HH:mm:ss", null);
+            //Debug.WriteLine(date1);
+            DateTime date2 = DateTime.ParseExact(ViewBag.izlazak, "dd'.'MM'.'yyyy - HH:mm:ss", null);
+            //Debug.WriteLine(date2);
+            TimeSpan interval = date2 - date1;
+            ViewBag.interval = interval;
+            //Debug.WriteLine(interval);
+           
+
+            return View();
+        }
+    }
+}
---
+++ RadniSati/Models/Sati.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RadniSati.Models
+{
+    public class Sati
+    {
+        public int SifraZaposlenika
+        { get; set; }
+        public string ImeZaposlenika
+        { get; set; }
+        public string PrezimeZaposlenika
+        { get; set; }
+        public int Ulazak
+        { get; set; }
+        public int Izlazak
+        { get; set; }
+        public int Razlika
+        { get; set; }
+        public int Visak
+        { get; set; }
+
+        public Sati(/*int sifra, string ime, string prezime,*/ int ulazak, int izlazak)
+        {
+            //this.SifraZaposlenika = sifra;
+            //this.ImeZaposlenika = ime;
+            //this.PrezimeZaposlenika = prezime;
+            this.Ulazak = ulazak;
+            this.Izlazak = izlazak;
+
+        }
+
+        public Sati()
+        {
+                
+        }
+    }
+}
