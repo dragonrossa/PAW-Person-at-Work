a9ab17106399f5e4bcc9093f6f721e54c913402c
Rosana Đuga <rodjuga@gmail.com>
2019-10-31 12:19:57 +0100

    Insert of working hours into table dbo.UnosSati, TempData inserts, unused datas removal

--- RadniSati/Controllers/RadniSatiController.cs
+++ RadniSati/Controllers/RadniSatiController.cs
@@ -30,7 +30,7 @@ namespace RadniSati.Controllers
         }
 
 
-        // GET: RadniSati
+       
         public ViewResult UnosSati()
         {
            
@@ -41,9 +41,15 @@ namespace RadniSati.Controllers
         public ViewResult UnosSati(FormCollection form)
         {
             string username = form["Username"];
-            Debug.WriteLine(username);
+            ViewBag.username = form["Username"];
+            //proslijediti na sljedeći ViewResult
+            TempData["Username"] = form["Username"];
+            //Debug.WriteLine(username);
             string password = form["Password"];
-            Debug.WriteLine(password);
+            //Debug.WriteLine(password);
+
+
+            //connect na bazu
             cnn = new SqlConnection(connectionString: RadniSati.ConnectionString);
             cnn.Open();
             
@@ -110,7 +116,29 @@ namespace RadniSati.Controllers
             DateTime date1 = DateTime.ParseExact(ViewBag.ulaz, "yyyy'-'MM'-'dd'T'HH':'mm':'ss", null);
             DateTime date2 = DateTime.ParseExact(ViewBag.izlaz, "yyyy'-'MM'-'dd'T'HH':'mm':'ss", null);
 
-            
+
+
+            string Ulazak = DateTime.Now.ToString("yyyy'-'MM'-'dd HH:mm:ss");
+            string Izlazak = DateTime.Now.AddHours(8).ToString("yyyy'-'MM'-'dd HH:mm:ss");
+
+            string datum = date1.ToString("dd-MM-yyyy"); //unos u bazu - datum
+            string ul = date1.ToString("yyyy-MM-dd HH:mm:ss"); //unos u bazu - ulazni broj
+            string iz = date2.ToString("yyyy-MM-dd HH:mm:ss"); //unos u bazu - izlazni broj
+
+            string ul2 = date1.ToString("dd-MM-yyyy") + " u " + date1.ToString("HH:mm:ss");
+            string iz2 = date2.ToString("dd-MM-yyyy") + " u " + date2.ToString("HH:mm:ss");
+
+
+            Debug.WriteLine(datum);
+            Debug.WriteLine(ul);
+            Debug.WriteLine(iz);
+
+
+            TempData["Datum"] = datum;
+            TempData["Ulazak"] = ul;
+            TempData["Izlazak"] = iz;
+
+
             var usCulture = new CultureInfo("en-US");
             var date3 = date1;
             var date4 = date2;
@@ -119,18 +147,101 @@ namespace RadniSati.Controllers
             var str1 = gmt1Date.ToString("dd MMM yyyy HH:mm:ss", usCulture);
             var str2 = gmt2Date.ToString("dd MMM yyyy HH:mm:ss", usCulture);
 
-            Debug.WriteLine(str1);
-            Debug.WriteLine(str2);
 
-            ViewBag.ulazak = str1;
-            ViewBag.izlazak = str2;
+            ViewBag.ulazak = ul2;
+            ViewBag.izlazak = iz2;
           
             TimeSpan interval = date4 - date3;
-            ViewBag.interval = interval;
+            ViewBag.interval = interval; //unos u bazu - ukupan broj sati
             TimeSpan span = new TimeSpan(08, 00, 00);
-            TimeSpan visak = interval - span;
-            ViewBag.visak = visak;
+            TimeSpan visak = interval - span; 
+            ViewBag.visak = visak;  //unos u bazu - visak, razlika
+
+            TempData["Razlika"] = interval;
+            TempData["Visak"] = visak; 
+
+
+            //connect na bazu
+            cnn = new SqlConnection(connectionString: RadniSati.ConnectionString);
+            cnn.Open();
+
+            SqlCommand command = new SqlCommand("SELECT u.Šifra, u.ImeZaposlenika,u.PrezimeZaposlenika, u.TipZaposlenika from dbo.UnosSati u JOIN dbo.Login l on u.Šifra=l.ŠifraZaposlenika WHERE l.Username=@username", cnn);
+
+            command.Parameters.AddWithValue("@username", TempData["Username"]);
+       
+
+            int result = command.ExecuteNonQuery();
+
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+
+                    TempData["sifra"] = reader[0].ToString();
+                    TempData["ime"] = reader[1].ToString();
+                    TempData["prezime"] = reader[2].ToString();
+                    TempData["tipZaposlenika"] = reader[3].ToString();
+
+                }
+            }
+
+
+            Debug.WriteLine("Connection Closed!");
+            cnn.Close();
+
+            Debug.WriteLine(TempData["Sifra"]);  //sifra za unos
+            Debug.WriteLine(TempData["Ime"]);  //ime za unos
+            Debug.WriteLine(TempData["Prezime"]); //prezime za unos
+            Debug.WriteLine(TempData["Datum"]); //datum za unos
+            Debug.WriteLine(TempData["Ulazak"]); //ulazak za unos
+            Debug.WriteLine(TempData["Izlazak"]); //izlazak za unos
+            Debug.WriteLine(TempData["Razlika"]); //razlika za unos
+            Debug.WriteLine(TempData["Visak"]); //visak za unos
+            Debug.WriteLine(TempData["TipZaposlenika"]); //tip zaposlenika za unos
+
+
+
+            //Upis u tablicu podatke o korisniku
+
+            try
+            {
+
+
+                cnn = new SqlConnection(connectionString: RadniSati.ConnectionString);
+            cnn.Open();
+
+            SqlCommand unos = new SqlCommand("INSERT INTO dbo.UnosSati VALUES(@sifra,@imeZaposlenika,@prezimeZaposlenika,@datum,@ulazak, @izlazak,@razlika,@visak,@tipZaposlenika)", cnn);
+
+            unos.Parameters.Add("@sifra", SqlDbType.Int).Value = TempData["Sifra"];
+            unos.Parameters.Add("@imeZaposlenika", SqlDbType.NChar).Value = TempData["Ime"];
+            unos.Parameters.Add("@prezimeZaposlenika", SqlDbType.NChar).Value = TempData["Prezime"];
+            unos.Parameters.Add("@datum", SqlDbType.Date).Value = TempData["Datum"];
+            unos.Parameters.Add("@ulazak", SqlDbType.DateTime).Value = TempData["Ulazak"];
+            unos.Parameters.Add("@izlazak", SqlDbType.DateTime).Value = TempData["Izlazak"];
+            unos.Parameters.Add("@razlika", SqlDbType.Time).Value = TempData["Razlika"];
+            unos.Parameters.Add("@visak", SqlDbType.Time).Value = TempData["Visak"];
+            unos.Parameters.Add("@tipZaposlenika", SqlDbType.NChar).Value = TempData["tipZaposlenika"];
+            unos.ExecuteNonQuery();
+
+
+           
+            cnn.Close();
+
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+            finally {
+
+                Debug.WriteLine("Podaci upisani!");
+            }
+
             return View();
+
+           
         }
     }
 }
